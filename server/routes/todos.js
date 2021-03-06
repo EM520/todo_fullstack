406b39159dcc6642dbb04560c978c0e01b857268
punchcode <punchcode@punchcodes-MacBook-Pro.local>
2021-03-06 12:10:32 -0500

    added proxy

--- server/routes/todos.js
+++ server/routes/todos.js
@@ -4,19 +4,20 @@ const router = express.Router()
 import conn from '../db-connection.js'
 
 router.get('/todos', (req, res) => {
-    const userId = req.params.userId
-    const todos = await conn.raw(`
-        SELECT
-        todos.todo_id ,
-        todos.description,
-        todos.status,
-        FROM todos
-        INNER JOIN users ON todos.todo_id = users.id;
-    `)
-    const rows = todos.rows
-    console.log(rows)
-    res.json(rows)
-    // res.json({message:'testing todos routes'})
+    
+    res.json({message:'testing todos routes'})
 })
 
 export default router
+
+// //     SELECT
+//     //     todos.todo_id ,
+//     //     todos.description,
+//     //     todos.status,
+//     //     FROM todos    //     INNER JOIN users ON todos.todo_id = users.id;   const rows = todos.rows
+     
+    // const userId = req.params.userId
+    // const todos = await conn.raw(`
+    // SELECT * FROM todos
+    // `)
+    // res.json(todos)
