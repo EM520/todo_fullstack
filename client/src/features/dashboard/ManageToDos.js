e90fe5f4a1136f905ca3d13b636a1286da7c4b0c
punchcode <punchcode@punchcodes-MacBook-Pro.local>
2021-03-09 00:48:59 -0500

    update

--- client/src/features/dashboard/ManageToDos.js
+++ client/src/features/dashboard/ManageToDos.js
@@ -1,37 +1,43 @@
+import React, { useEffect, useState } from "react";
+import "../../App.css";
+import ToDoList from "./ToDoList";
+import { useSelector, useDispatch } from "react-redux";
+import { selectToDos, getToDos, addToDo } from "./todoSlice";
 
-import React, {useEffect,useState} from 'react';
-import '../../App.css';
-import ToDoList from './ToDoList';
-import {  useSelector, useDispatch } from "react-redux";
-import {selectToDos, getToDos, addToDo} from './todoSlice';
+export default function ManageToDos() {
+  const todos = useSelector(selectToDos);
+  console.log(todos, "m");
 
-export default  function ManageToDos() {
-const todos = useSelector(selectToDos)
-console.log(todos ,'m')
+  const dispatch = useDispatch();
+  useEffect(() => {
+    console.log("called once....");
+    dispatch(getToDos());
+  }, []);
+  const [inputTodo, setInputTodo] = useState("");
 
-const dispatch = useDispatch()
-useEffect(() => {
-  dispatch(getToDos())
-}, []);
-const [inputTodo, setInputTodo] = useState('');
-
-function handleSubmit(e) {
-  e.preventDefault()
-
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log("submit the todo....");
     // e.preventDefault();
-    console.log(inputTodo,'input')
+    console.log(inputTodo, "input");
     dispatch(addToDo(inputTodo));
-    setInputTodo('')
-}
+    setInputTodo("");
+  }
 
-  return <div>
-    <form className="toDoList" onSubmit={handleSubmit}>
-        <input type="text" className="todo-entry" 
-         onChange={(e) => setInputTodo(e.target.value)} 
-        value = {inputTodo}
-        className ="inputTodo"
-        placeholder="Enter Your ToDo Here"/>
-    </form> 
-    <ToDoList todo={todos}/>
-  </div>
+  return (
+    <div>
+      <form className="toDoList" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          className="todo-entry"
+          onChange={(e) => setInputTodo(e.target.value)}
+          value={inputTodo}
+          className="inputTodo"
+          placeholder="Enter Your ToDo Here"
+        />
+      </form>
+      {/* {todos.map(todos => <ToDoList todos ={todos}/>)} */}
+      <ToDoList todos={todos} />
+    </div>
+  );
 }
--- server/routes/todos.js
+++ server/routes/todos.js
@@ -1,62 +1,71 @@
 import express from "express";
 const router = express.Router();
+let globalUserID;
 import conn from "../db-connection.js";
 router.use(express.json());
 router.use(express.urlencoded({ extended: false }));
 
-
 // search request by description
 router.get("/todosearch/:userId/:desc", async (req, res) => {
-  console.log(req.params, 'body')
-  console.log(`"%${req.params.desc}%"`, req.params.userId , '100')
+  console.log(req.params, "body");
+  console.log(`"%${req.params.desc}%"`, req.params.userId, "100");
   const userId = req.params.userId;
   const desc1 = req.params.desc;
   // console.log(desc1,userId, 'all')
   // const {description} = req.body;
-  const todos = await conn.raw(`
+  const todos = await conn.raw(
+    `
         SELECT * FROM todos
         WHERE description LIKE ?
-        AND user_id=?
+        AND user_id
     `,
     [desc1, userId]
-    );
+  );
   const rows = todos.rows;
-  res.json(rows)
-  console.log(rows)
-//   res.json({message:'testing todos search routes'});
- 
+  res.json(rows);
+  console.log(rows);
+  //   res.json({message:'testing todos search routes'});
 });
 
 // Search Todos by description
 //Read and get Todos  by user_id
 router.get("/todos", async (req, res) => {
-    const userId = req.user.id;
-    const todos = await conn.raw(`
-          SELECT * 
-          FROM todos
-          WHERE user_id=?    
-      `,
-      [userId]
-      );
-    const rows = todos.rows;
-    console.log(rows);
-    res.json(rows);
-    // res.json({message:'testing todos routes'})
-  });
+  // const userId = req.user.id;
+  const sqlDependencies = [req.user.id];
+  globalUserID = req.user.id;
+  console.log(req.user.id, globalUserID, "id");
+
+  const description = req.query.description;
+  let sql = `
+    SELECT * 
+    FROM todos
+    WHERE user_id=?    
+    
+    `;
+  if (description) {
+    sql += `AND description LIKE ?`;
+    sqlDependencies.push("%" + description + "%");
+  }
+  const todos = await conn.raw(sql, sqlDependencies);
+  const rows = todos.rows;
+  console.log(rows);
+  res.json(rows);
+});
 
 // Create new Todos
 router.post("/todos", async (req, res) => {
   const userId = req.user.id;
-  const { description} = req.body;
+  console.log(userId, "id pls!");
+  const { description } = req.body;
   await conn.raw(
     `
         INSERT INTO todos (description, status,user_id)
         VALUES( ?,?,?)
     `,
-    [description,"active",userId]
+    [description, "active", userId]
   );
   res.json({ message: "Todos added" });
-//   res.json({message:'testing todos added'})
+  //   res.json({message:'testing todos added'})
 });
 
 // update Request
@@ -74,26 +83,25 @@ router.post("/todos", async (req, res) => {
 //   });
 
 // Update Todos
-  router.patch("/todos/:todoId", async (req, res) => {
-    const todoId = req.params.todoId;
-    await conn('todo')
-        .where({id:todoId})
-        .update(req.body)       
-    res.json({message:'Todos updated'})
-  });
-
+router.patch("/todos/:todoId", async (req, res) => {
+  console.log("request body", req.body);
+  const todoId = req.params.todoId;
+  await conn("todos").where({ id: todoId }).update(req.body);
+  res.json({ message: "Todos updated" });
+});
 
 //Backend Delete Todos
 router.delete("/todos/:todoId", async (req, res) => {
-    const todoId = req.params.todoId;
-    
-    await conn.raw(`
-          DELETE FROM todos
-          WHERE id =? 
-          `,
-          [todoId]);
-    res.json({message:'Todos deleted'})
-  });
+  const todoId = req.params.todoId;
 
+  await conn.raw(
+    `
+          DELETE FROM todos
+          WHERE id =?  
+          `,
+    [todoId]
+  );
+  res.json({ message: "Todos deleted" });
+});
 
 export default router;
